508ce668da16a43de48ebf28978a1337044d3fa5
Kirsten McAulay <mcaulay.kirsten@gmail.com>
2016-07-15 16:29:44 -0400

    updating

---
+++ scripts/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+	$("#mm").hover(function(){
+		$("#show1").css("display", "inline-block");
+	}, function(){
+		$("#show1").css("display", "none");
+	})
+
+	$("#roam").hover(function(){
+		$("#show2").css("display", "inline-block");
+	}, function(){
+		$("#show2").css("display", "none");
+	})
+
+	$("#juke").hover(function(){
+			$("#show3").css("display", "inline-block");
+		}, function(){
+			$("#show3").css("display", "none");
+		})
+
+
+
+
+})
